c532e0acd554dff181a278f8988b7894753c76f8
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-01 17:02:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,11 @@ from scipy.stats import lognorm
 chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # оценка параметров логнормального распределения
-    s, loc, scale = lognorm.fit(x, floc=0)
-    # оценка коэффициента a
-    a = np.log(np.mean(x) - 419) - 0.5 * s**2
-    return a
+# Оценка параметров логнормального распределения
+mu = np.mean(np.log(x - 419))
+sigma = np.std(np.log(x - 419))
+
+# Оценка параметра "a"
+a = np.exp(mu + sigma ** 2 / 2)
+
+return a
