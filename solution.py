dccd39f1a20f39a9864deb7600c52f3a6a6df1de
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-01 16:36:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import lognorm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    # оценка параметров логнормального распределения
+    s, loc, scale = lognorm.fit(x, floc=0)
+    # оценка коэффициента a
+    a = np.log(np.mean(x) - 419) - 0.5 * s**2
+    return a
