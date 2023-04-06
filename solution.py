aac27fadf6f8a948f05a8d836309d239bbf9efbd
khmyrov-a243 <128284168+khmyrov-a243@users.noreply.github.com>
2023-04-06 18:21:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,8 @@ import numpy as np
 chat_id = 973327975 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-  n = len(x)
-  mu = np.mean(np.log(x)) # оценка параметра μ
-  sigma2 = np.var(np.log(x)) # оценка параметра σ^2
-  a = np.exp(mu + sigma2/2) - 419 # оценка коэффициента a
+    n = len(x)
+    mu = np.mean(np.log(x)) # оценка параметра μ
+    sigma2 = np.var(np.log(x)) # оценка параметра σ^2
+    a = np.exp(mu + sigma2/2) - 419 # оценка коэффициента a
 return a
